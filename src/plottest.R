3418c705de7372da41162a3d2c23f36f4694579b
hanwenhsu <133752453+hanwenhsu@users.noreply.github.com>
2023-06-28 14:10:53 +0200

    add gc_63_11.xlsx

--- src/plottest.R
+++ src/plottest.R
@@ -1,8 +1,9 @@
+rm(list = ls())
 library(dplyr)
 library(ggplot2)
 
 p <- "./data/Grain_Counting/gc_57_11.xlsx"
-df <- readxl::read_xlsx(p, sheet=5) %>% 
+df <- readxl::read_xlsx(p, sheet=1) %>% 
   mutate(across(starts_with("kernel"),function(x)as.character(x))) %>% 
   tidyr::pivot_longer(starts_with("kernel"),names_to = "kernel.type",values_to = "floret.pos") %>% 
   mutate(floret.pos=strsplit(floret.pos,",")) %>% 
@@ -20,3 +21,41 @@ df %>%
   theme(legend.position = "none",
         strip.background = element_blank(),
         panel.grid.major.x = element_line())
+
+df %>%
+  filter(floret.pos != 0) %>%
+  ggplot( aes(x=floret.pos, y=spike, size = kernel.type, color = kernel.type)) +
+  geom_point(alpha=0.7) +
+  #scale_size(range = c(1.4, 19), name="Population (M)") +
+  #scale_color_viridis(discrete=TRUE, guide=FALSE) +
+  theme_bw() +
+  theme(legend.position="bottom")
+
+
+df_multi <- purrr::map_dfr(1:6,~{
+  file<- readxl::read_xlsx(p, sheet = .x)
+  file <- file %>% mutate(across(starts_with("kernel"),function(x)as.character(x))) 
+  file <- file %>% tidyr::pivot_longer(starts_with("kernel"),names_to = "kernel.type",values_to = "floret.pos") 
+  file <- file %>% mutate(floret.pos=strsplit(floret.pos,","))  
+  file <- file %>% tidyr::unnest(floret.pos) 
+  file <- file %>% mutate(floret.pos=as.numeric(floret.pos) %>%replace(., is.na(.), 0))
+  # file <- file %>% mutate(kernel.type = factor(kernel.type, 
+  #                                              levels = c("kernel.S","kernel.M","kernel.L"),
+  #                                              ordered = TRUE))
+  return(file)
+})
+
+library(viridis)
+
+df_multi %>%
+  filter(floret.pos != 0) %>%
+  ggplot(aes(x=floret.pos, y=spike)) +
+  geom_point(alpha = 0.7,aes(size = kernel.type, fill = kernel.type)) +
+  facet_wrap(~rep)+
+  theme_bw() +
+  scale_fill_viridis_c(guide="legend",breaks=c(1,2,5))+
+  theme(legend.position="bottom",
+        strip.background = element_blank(),
+        panel.grid.major = element_line())
+# rep2_spike11&14 --> two kernels in floret one
+# rep4_spike 5 --> two kernels in floret one
---
+++ src/readxlsxear.R
@@ -0,0 +1,49 @@
+rm(list = ls())
+library(dplyr)
+library(ggplot2)
+readx<- function(p,sh){
+  df <- readxl::read_xlsx(p,sheet = sh) %>% 
+    mutate(across(starts_with("kernel"),function(x)as.character(x))) %>% 
+    tidyr::pivot_longer(starts_with("kernel"),names_to = "kernel.type",values_to = "floret.pos") %>% 
+    mutate(floret.pos=strsplit(floret.pos,",")) %>% 
+    tidyr::unnest(floret.pos) %>% 
+    mutate(floret.pos=as.numeric(floret.pos) %>%replace(., .==0, NA),
+           kernel.size=factor(kernel.type,levels=paste0("kernel.",c("S","M","L"))) %>% as.numeric() %>% 
+             # create contrast
+             ifelse(.==3,5,.))
+  
+}
+
+plot_fun <- function(df){
+  # find floret where more than two types were recorded for same position  
+  sdf <- df %>% group_by(rep,spike,floret.pos) %>% 
+    summarise(n=n()) %>% 
+    filter(n>1) %>% 
+    mutate(lab=paste0("Check\n(Nsp,Nf)\n(",spike,',',floret.pos,')'))
+  
+  p <- df %>%
+    ggplot(aes(floret.pos,spike))+
+    geom_point(alpha=.4,aes(size=kernel.size,fill=kernel.size),
+               shape=21)+
+    facet_wrap(~rep)+
+    theme_bw()+
+    ggrepel::geom_label_repel(data = sdf, mapping=aes(floret.pos,spike, label = lab),color="red",
+                              box.padding = 1,size=3)+
+    scale_fill_viridis_c(guide = "legend",breaks = c(1, 2, 5),
+                         labels = paste0("kernel.",c("S","M","L"))) +
+    scale_size_continuous(breaks = c(1, 2, 3), range = c(1, 5),
+                          labels = paste0("kernel.",c("S","M","L")))+
+    theme(panel.grid.minor.x=element_blank(),strip.background = element_blank(),
+          legend.position = "bottom")
+  return(p)
+  
+}
+# -------------------------------------------------------------------------
+p <- "data/Grain_Counting/gc_63_11.xlsx"
+
+graindf <- purrr::xxmap_dfr(1:length(readxl::excel_sheets(p)),~{
+  readx(p,.x)
+}) %>% na.omit() 
+
+graindf %>% plot_fun()
+
